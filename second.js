33d7014f84d1ec7de0c6143d97782f78a98b6ba4
edmundqi <edmund2015@qq.com>
2018-11-17 21:33:33 +0800

    button
    button added

---
+++ second.js
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+
+<script>
+document.write("<h1>你好</h1>");
+document.write("<p>你好</p>");
+button type="button" onclick="alert('欢迎!')">点我!</button>
+<button onclick="displayDate()">The time is?</button>
+</script>
+</body>
+</html>
